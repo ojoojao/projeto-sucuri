e84e1b60f00a42c6e5b5b144f075a7324aaa4ef2
João Saldanha <jlucassaldanha@gmail.com>
2025-04-24 18:46:11 -0300

    apaga run_files

--- run_files/storm.py
+++
@@ -1,9 +0,0 @@
-t = "C:/Users/jluca/Documents/python/sucuri"
-
-print("Caminho original:", t)
-for i, l in enumerate(t[::-1]):
-    if l == '/':       
-        t = t[::-1][i:][::-1]
-        break
-
-print("Caminho novo:", t)
