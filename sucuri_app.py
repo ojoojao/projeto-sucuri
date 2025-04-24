0b7c51b5e94ae186ff9b7aea23d5daa0ceb90e65
João Saldanha <jlucassaldanha@gmail.com>
2025-04-24 17:20:43 -0300

    organização

---
+++ sucuri_app.py
@@ -0,0 +1,19 @@
+import sys
+from PySide6.QtWidgets import QApplication
+from sucuri_run.interface.IDE import MainWindow
+
+def run_main_app():
+    sys.argv += ['-platform', 'windows:darkmode=2']
+
+    app = QApplication(sys.argv)
+    app.setStyle("Fusion")
+
+    window = MainWindow()
+    window.show()
+
+    app.exec()
+
+    app.shutdown()
+
+if __name__ == "__main__":
+    run_main_app()
---
+++ sucuri_run/core/sintaxe.py
@@ -0,0 +1,68 @@
+a_palavras_reservadas = {
+    'Falso'      : 'False',
+    'Nada'       : 'None',
+    'Verdade'    : 'True',
+    ' tambem '     : ' and ',
+    ' ou '         : ' or ',
+    'classe '      : 'class',
+    ' nao '        : ' not ',
+    'def '       : 'def ',
+    ' e '         : ' is ',
+}
+
+r_palavras_reservadas = {
+    ' como '       : ' as ',
+    'quebra'     : 'break',
+    'continua'   : 'continue',
+    'del'        : 'del',
+    'oucaso'     : 'elif',
+    'senao:'       : 'else:',
+    'exceto'     : 'except',
+    'entao'      : 'finally',
+    'por'        : 'for',
+    'de '         : 'from ',
+    'global '     : 'global ',
+    'caso'       : 'if',
+    'importa '    : 'import ',
+    ' em '        : ' in ',
+    'passa'      : 'pass',
+    'projeta'    : 'raise',
+    'retorna '    : 'return ',
+    'tenta'      : 'try',
+    'durante'    : 'while',
+    'com '       : 'with '
+}
+
+funcoes = {
+    'todo'            : 'all',
+    'bool'             : 'bool', 
+    'metododeclasse'  : 'classmethod',
+    'dicio'            : 'dict', 
+    'dir('             : 'dir(',
+    'enumera('         : 'enumerate(',
+    'exec('            : 'exec(',
+    'filtro('          : 'filter(',
+    'flutuante'        : 'float' ,
+    'formata('         : 'format(',
+    'globais('         : 'globals(',
+    'hexa'             : 'hex',
+    'entrada('         : 'input(',
+    'int'              : 'int',
+    'tam('             : 'len(',
+    'lista'            : 'list',
+    'abre('            : 'open(',
+    'mostra('          : 'print(',
+    'faixa('           : 'range(',
+    'arredonda('       : 'round(',
+    'define('          : 'set(',
+    'metodostatico'   : 'staticmethod',
+    'txt'              : 'str',
+    'soma('            : 'sum(',
+    'super('           : 'super(',
+    'tupla'            : 'tuple',
+    'tipo('            : 'type(',
+}
+
+
+operadores = ['(', ')', '[',']',"{",'}',"@", ",", ";", ":", "!", ".", '=', '-', '>', '<', '+', '*', '&', "/", '"', "'", "%", "$", "?", "´", "~", "^", "\\", '#']
+
